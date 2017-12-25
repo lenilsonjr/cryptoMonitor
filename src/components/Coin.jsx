b33bea3de225bd2afc586e5ff43e89cd646fb2f0
Lenilson Júnior <me@lenilson.codes>
2017-12-25 09:58:03 -0300

    :wrench: Changed default currency comparator to BTC

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -10,7 +10,7 @@ class Coin extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      OtherCurrency: 'USD',
+      OtherCurrency: 'BTC',
 
       Change: 0,
       ChangeIcon: 'glyphicon glyphicon-minus',
